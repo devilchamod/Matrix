4ba611c2491a2add9d0975fa4b34456e5718c2c7
devilchamod <161329983+devilchamod@users.noreply.github.com>
2026-09-12 12:54:10 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/bythnqwriaiiavlwacby';
+const SUPABASE_URL = 'https://bythnqwriaiiavlwacby.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'sb_publishable_AyWpbYzR7LjpFi8QtdXh4w_fromsAMp';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
